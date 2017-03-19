6d745f9e8c03c0302db5bb914216efd3b55a8642
Darwin Truong <darwin.truong@outlook.com>
2017-03-19 00:43:54 -0400

    rrrrround 1

---
+++ bot/bot.js
@@ -0,0 +1,62 @@
+const Discord = require('discord.js');
+var register = require('./commands/register.js');
+//var messager = require('./commands/messager.js');
+var net = require('net');
+const bot = new Discord.Client();
+
+const HOST = '10.16.185.66';
+const PORT = '5000';
+
+var client = new net.Socket();
+
+client.connect(PORT, HOST, () => {
+	console.log("CONNECTED TO : " + HOST + ":" + PORT);
+});
+
+bot.on('message', (message) => {
+
+	if (message.content == '!signup') {
+
+		register.signUp(message);
+
+	} else if (message.content.startsWith('!m ')) {
+
+		var msg = getMessageContent(message.content);
+		var name = message.content.split(' ')[1];
+
+		sendMessage('m', name, msg.trim());
+
+	} else if (message.content.startsWith('!r ')) {
+
+		var msgIndex = message.content.indexOf(' ');
+		var msg = message.content.substring(msgIndex).trim();
+
+		sendMessage('r', '', msg);
+	}
+
+});
+
+client.on('data', (data) => {
+	console.log("RECIEVED : " + data);
+})
+
+function sendMessage(type, name, message) {
+
+	messageObj = {
+		type: type,
+		name: name,
+		message: message
+	};
+
+	var msgObjStr = JSON.stringify(messageObj);
+	console.log("WROTE: " + msgObjStr + " TO " + HOST + ":" + PORT);
+	client.write(msgObjStr);
+}
+
+function getMessageContent(msg) {
+	var first = msg.indexOf(' ');
+	var second = msg.indexOf(' ', first + 1);
+	return msg.substring(second);
+}
+
+bot.login('MjkyODI1NDE2NDIxNjA1Mzc2.C69qoA.UepTYH9OIClfrqZLZcLZkDUTzOY');
---
+++ bot/commands/register.js
@@ -0,0 +1,22 @@
+signedUp = {};
+
+module.exports =  {
+	signUp: (message) => {
+
+		var author = message.author;
+		var id = author.id;
+
+		if (!signedUp.id) {
+			console.log(message.author.id + ": " + message.author.username + " has been registered");
+			signedUp.id = id;
+			author.sendMessage(
+						"Hi, you have been registered for Facebook messaging."
+					);
+		} else {
+			author.sendMessage(
+					"You are already signed up."
+				);
+		}
+
+	}
+};
